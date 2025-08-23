8bf20ea2cddc3618a6640cab4d060fefc442cc6a
GalobelWang <hkuzpwang@gmail.com>
2025-08-23 09:28:28 +0000

    Deploying to gh-pages from @ GalobelWang/GalobelWang.github.io@e77ac87bcf19c9de47f249c3c8d577e4998d019f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -378,9 +378,9 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+      },{id: "projects-probability-theory-and-mathematical-statistics",
+          title: 'Probability Theory and Mathematical Statistics',
+          description: "a brief introduction",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
