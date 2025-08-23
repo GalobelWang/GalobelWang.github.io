b3537a17b6edb7699f3df24214f5379eef9f84f1
GalobelWang <hkuzpwang@gmail.com>
2025-08-23 03:50:12 +0000

    Deploying to gh-pages from @ GalobelWang/GalobelWang.github.io@e3eeae4dc3e40c70bf2fc91834597603e10e5c93 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
+        },{id: "nav-notes",
+          title: "notes",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
