089a402a8184d527feb678e9c59a6356d3425705
GalobelWang <hkuzpwang@gmail.com>
2025-08-23 10:14:40 +0000

    Deploying to gh-pages from @ GalobelWang/GalobelWang.github.io@52a5b25ff7a69decb6cf1ff7b11f7afe3d7696bd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -427,11 +427,6 @@ ninja.data = [{
           title: 'Series',
           description: "a brief introduction",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/Series%20-%20Copy/";
-            },},{id: "projects-series",
-          title: 'Series',
-          description: "a brief introduction",
-          section: "Projects",handler: () => {
               window.location.href = "/projects/Series/";
             },},{id: "projects-stability-theory",
           title: 'Stability Theory',
