adc5eb90dfb6dc3cfb851074862a932dab01bc73
GalobelWang <hkuzpwang@gmail.com>
2025-08-23 12:47:58 +0000

    Deploying to gh-pages from @ GalobelWang/GalobelWang.github.io@f44829d62a6a7804f085eb87ea5e4bffdca80cfb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -380,7 +380,7 @@ ninja.data = [{
         },
       },{id: "projects-abstract-algebra",
           title: 'Abstract Algebra',
-          description: "a brief introduction",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/AA/";
             },},{id: "projects-complex-algebraic-geometry",
@@ -390,12 +390,12 @@ ninja.data = [{
               window.location.href = "/projects/AG/";
             },},{id: "projects-complex-analysis",
           title: 'Complex Analysis',
-          description: "a brief introduction",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/CA/";
             },},{id: "projects-fourier-analysis",
           title: 'Fourier Analysis',
-          description: "a brief introduction",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/FourierA/";
             },},{id: "projects-galois-theory",
